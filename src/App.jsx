c235a6c8a86bcaacd1afc7682a4673a60709c4f3
Nathan Wood <130803157+nathnwod@users.noreply.github.com>
2026-02-06 22:28:48 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="main">
+      <div className="main-container">
+        <h1>Nathan Wood</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
